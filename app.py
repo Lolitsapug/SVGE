941782cb4246ade843a23e95c810fe1b2768d5ea
lolitsapug <daviddeng267@gmail.com>
2025-11-09 20:54:14 +0000

    port 80

--- app.py
+++ app.py
@@ -189,7 +189,7 @@ if __name__ == '__main__':
     print("=" * 50)
     
     # Use environment variables for production deployment
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 80))
     host = os.environ.get('HOST', '0.0.0.0')
     debug = os.environ.get('DEBUG', 'False').lower() == 'true'
     
